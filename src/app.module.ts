69205bf527e4913c5b37e54abc5b523b9f0c3821
beha757 <159917751+Behatotsamiy@users.noreply.github.com>
2026-02-10 14:32:40 +0500

    modules are installed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,62 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule } from './clients/clients.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BarbersModule } from './barbers/barbers.module';
+import { ServicesModule } from './services/services.module';
+import { AppointmentsModule } from './appointments/appointments.module';
+import { FeedbackModule } from './feedback/feedback.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const isProd = configService.get('NODE_ENV') === 'production';
+        const databaseUrl = configService.get('DATABASE_URL');
+        if (isProd && databaseUrl) {
+          return {
+            type: 'postgres',
+            url: databaseUrl,
+            autoLoadEntities: true,
+            synchronize: true,
+            logging: true,
+            ssl: {
+              rejectUnauthorized: false,
+            },
+            uuidExtension: 'pgcrypto',
+          };
+        }
+        return {
+          type: 'postgres',
+          host: configService.get('DB_HOST'),
+          port: +configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_NAME'),
+          autoLoadEntities: true,
+          synchronize: true,
+          logging: true,
+          uuidExtension: 'pgcrypto',
+        };
+      },
+    }),
+    
+     ClientsModule,
+    
+     BarbersModule,
+    
+     ServicesModule,
+    
+     AppointmentsModule,
+    
+     FeedbackModule],
   controllers: [AppController],
   providers: [AppService],
+
 })
 export class AppModule {}
